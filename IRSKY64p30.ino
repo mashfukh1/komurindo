e3f5a58b4df1921011d10cd4d675edd0df195fc8
muhammad mashfukh <34981773+mashfukh1@users.noreply.github.com>
2021-01-13 23:53:42 +0700

    Add files via upload

---
+++ IRSKY64p30.ino
@@ -0,0 +1,382 @@
+/*
+    LIBRARY
+*/
+#include <Adafruit_VC0706.h>
+#include "I2Cdev.h"
+#include "ADXL345.h"
+#include "Wire.h"
+
+/*
+    CONFIG CONSTANT VALUE
+*/
+#define GPS_BAUD    38400
+#define TRX_BAUD    57600
+#define IMU_BAUD    57600
+#define TRX_INTERVAL    26000
+#define GPS_RATE    1000000
+#define PARASUT_ON  300
+
+#define IMU_CONF    "#osct#o0"
+#define IMG_RES_CONF VC0706_160x120
+// TELCOM
+#define START       'A'       //mulai transmisi
+#define STOP        'B'       //Stop transmisi
+#define PICTURE     'C'       //ambil gambar
+#define NULL        '0'       //bersihkan telcom
+
+//TELCOM ADV
+#define CAM_STAT    'Z'
+#define IMU_STAT    'Y'
+#define GPS_STAT    'X'
+
+//INDEX SETUP
+#define CAM_INDEX   0
+#define GPS_INDEX   1
+#define IMU_INDEX   2
+
+/*
+    PIN CONFIGURATION
+*/
+#define GPS   Serial1
+#define TRX   Serial
+#define IMU   Serial2
+#define CAMERA_PIN  Serial3
+#define PARASUT_SENS  A8
+/*
+    GLOBAL VARIABEL
+*/
+struct dataGPS{
+  float
+    longi,    // longitude
+    lati,     // latitude
+    alti,     // altitude
+    speed,    // speed
+    head;     // heading
+
+  int
+    sat;
+
+  uint8_t
+    fixQ,
+    hour,
+    minute,
+    second;
+} dataGPS;
+
+
+bool
+  camStat = false,
+  gpsStat = false,
+  imuStat = false;
+
+uint8_t imgSize;
+
+char camVersion[100];
+
+volatile unsigned char
+    telcom = NULL;
+
+unsigned long gpsRTime, imuRTime;
+
+/*
+    PEWARISAN SIFAT
+*/
+Adafruit_VC0706 CAM = Adafruit_VC0706(&CAMERA_PIN);
+ADXL345 accel;
+/*
+    FUNCTION PROTOTYPE
+*/
+void setup(void);
+void loop(void);
+void serialEvent(void);
+void serialEvent1(void);
+void initDataGPS(void);
+void camSetup(void);
+void camCheck(void);
+void imuSetup(void);
+void imuCheck(void);
+void gpsCheck(void);
+void updateGPS(void);
+void sendGPS(void);
+void sendIMU(void);
+void takePicture(void);
+void tryOut(void);
+void clrTelcom(void);
+uint16_t parasutStat(void);
+
+/*
+    FUNCTION IMPELENTATION
+*/
+
+//Konfigurasi arduino
+void setup(void){
+  Wire.begin();
+  TRX.begin(TRX_BAUD);
+  GPS.begin(GPS_BAUD);
+  pinMode(PARASUT_SENS, INPUT_PULLUP);
+  delay(500);
+  imuSetup();
+  camSetup();
+  accel.initialize();
+  accel.testConnection();
+  delay(100);
+  accel.setRange(ADXL345_RANGE_8G);
+  gpsRTime = imuRTime = micros();
+ }
+
+//fungsi utama
+void loop(void){
+  sendGPS();
+//  static unsigned long ctime;
+//  static String dataIMU;
+//  static int16_t ax, ay, az;
+//
+//  if(IMU.available()){
+//    dataIMU = IMU.readStringUntil('\n');
+//    IMU.print("#f");
+//    accel.getAcceleration(&ax, &ay, &az);
+//  }
+//    
+//  switch (telcom) {
+//    case NULL:
+//      delay(50);
+//      break;
+//
+//    case START:
+//      ctime = micros();
+//      if((ctime - gpsRTime) > GPS_RATE){
+//        sendGPS();
+//        gpsRTime = micros();
+//      }
+//      if((ctime - imuRTime) > TRX_INTERVAL){
+//        TRX.print("I,");
+//        TRX.print(ax);
+//        TRX.print(',');
+//        TRX.print(ay);
+//        TRX.print(',');
+//        TRX.print(az);
+//        TRX.print(',');     
+//        TRX.println(dataIMU);
+//        imuRTime = micros();
+//      }
+//      break;
+//
+//    case STOP:
+//        delay(50);
+//      break;
+//
+//    case PICTURE:
+//      takePicture();
+//      clrTelcom();
+//      telcom = START;
+//      break;
+//
+//    case CAM_STAT:
+//      camCheck();
+//      telcom = NULL;
+//      break;
+//
+//    case IMU_STAT:
+//      imuCheck();
+//      telcom = NULL;
+//      break;
+//
+//    case GPS_STAT:
+//      gpsCheck();
+//      telcom = NULL;
+//      break;
+//  }
+}
+
+// inisialisasi dataGPS
+void initDataGPS(void){
+  dataGPS.longi = 0;
+  dataGPS.lati = 0;
+  dataGPS.alti = 0;
+  dataGPS.speed = 0;
+  dataGPS.head = 0;
+  dataGPS.sat = 0;
+  dataGPS.hour = 0;
+  dataGPS.minute = 0;
+  dataGPS.second = 0;
+}
+
+//  Interrupt telecommand (Antena)
+void serialEvent2(void){
+  if(TRX.available()){
+    telcom = TRX.read();
+  }
+}
+
+void clrTelcom(void){
+  while(TRX.available())
+    (void)TRX.read();
+}
+
+// Interrupt GPS
+void serialEvent1(void){
+  while (GPS.available())
+    gpsENC(GPS.read());
+}
+
+//Konfigruasi dan inisialisasi kamera
+void camSetup(void){
+  char * ver;
+  if(CAM.begin())
+    camStat = 1;
+  else
+    camStat = 0;
+
+  ver = CAM.getVersion();
+  if(ver)
+    sprintf(camVersion, "%s", ver);
+  else
+    sprintf(camVersion, "%s", "Failed to get Version");
+
+  CAM.setImageSize(VC0706_160x120);
+  imgSize = CAM.getImageSize();
+  //delay(500);
+  //CAM.setBaud115200();
+}
+
+//Check status kamera
+void camCheck(void){
+  if (camStat){
+    TRX.println("Camera OK");
+
+    TRX.println("Camera Version: ");
+    TRX.print(camVersion);
+
+    TRX.print("Image size: ");
+    if (imgSize == VC0706_640x480) TRX.println("640x480");
+    else if (imgSize == VC0706_320x240) TRX.println("320x240");
+    else if (imgSize == VC0706_160x120) TRX.println("160x120");
+    else{
+      TRX.print(imgSize);
+      TRX.print("\t");
+      TRX.println("SIZE NOT RECOGNIZED");
+    }
+  }
+  else
+    TRX.println("Camera Failed, Check Wiring and Program");
+}
+
+void imuSetup(void){
+  IMU.begin(IMU_BAUD);
+  delay(100);
+
+  IMU.print(IMU_CONF);
+  delay(50);
+  IMU.print("#f");
+}
+
+void imuCheck(void){
+  long timeOut;
+
+  timeOut = millis();
+  delay(10);
+  IMU.print("#f");
+
+  imuStat = true;
+  while(IMU.available() < 0){
+    if(millis() - timeOut > 3000){
+      imuStat = false;
+      break;
+    }
+  }
+
+  if(imuStat){
+    TRX.print("IMU OK, data: ");
+    TRX.println(IMU.readStringUntil('\n'));
+  }
+  else
+    TRX.println("IMU Failed, Check Wiring and Program");
+}
+
+
+void gpsCheck(void){
+  if(gpsStat){
+    TRX.print("GPS OK: ");
+  }
+  else{
+    TRX.println("GPS FAILED, Check Wiring and Program!");
+  }
+}
+
+/*
+void updateGPS(void){
+  if(gpsENC.location.isValid()){
+    dataGPS.longi = gpsENC.location.lng();
+    dataGPS.lati = gpsENC.location.lat();
+  }
+
+  if(gpsENC.altitude.isValid())
+    dataGPS.alti = gpsENC.altitude.meters();
+
+  if(gpsENC.speed.isValid())
+    dataGPS.speed = gpsENC.speed.kmph();
+
+  if(gpsENC.course.isValid())
+    dataGPS.head = gpsENC.course.deg();
+
+  if(gpsENC.satellites.isValid())
+    dataGPS.sat = gpsENC.satellites.value();
+
+  if(gpsENC.time.isValid()){
+    dataGPS.hour = gpsENC.time.hour();
+    dataGPS.minute = gpsENC.time.minute();
+    dataGPS.second = gpsENC.time.second();
+  }
+}
+*/
+
+void sendGPS(void){
+  String packet;
+  TRX.print("G,");
+  TRX.print(dataGPS.longi, 6); TRX.print(",");
+  TRX.print(dataGPS.lati, 6); TRX.print(",");
+  TRX.print(dataGPS.alti, 2); TRX.print(",");
+  TRX.print(dataGPS.speed, 2); TRX.print(",");
+  TRX.print(dataGPS.head, 2); TRX.print(",");
+  TRX.print(dataGPS.sat); TRX.print(",");
+  TRX.print(dataGPS.fixQ); TRX.print(",");
+  TRX.print(dataGPS.hour); TRX.print(",");
+  TRX.print(dataGPS.minute); TRX.print(",");
+  TRX.print(dataGPS.second); TRX.print(",");
+  TRX.println(digitalRead(PARASUT_SENS));
+}
+
+void sendIMU(void){
+
+}
+
+void takePicture(void){
+  CAM.resumeVideo();
+  delay(50);
+  if (!CAM.takePicture())
+    TRX.println(0);
+
+  long imglen = CAM.frameLength();
+  uint8_t *buf;
+  long len;
+
+  TRX.println(imglen);
+
+
+  char ct = 0;
+
+  while(imglen > 0){
+    len = min(64, imglen);
+    buf = CAM.readPicture(len);
+
+    int i = 0;
+    imglen -= len;
+
+    TRX.write(buf, len);
+  }
+}
+
+uint16_t parasutStat(void){
+  return (analogRead(PARASUT_SENS));
+}
+
---
+++ lib.ino
@@ -0,0 +1,184 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char buff[100];
+int buffOfset = 0;
+uint8_t sum = 0;
+char sumVal [3];
+bool sumStat = false;
+char svIndex = 0;
+
+void procGPGGA(char *, struct dataGPS *);
+void procGPRMC(char *, struct dataGPS *);
+int chksum(char *);
+int toInt(char *, int);
+float parseLoc(char *, char *);
+void gpsENC(char );
+
+/*
+int main(void){
+  char GPS[] = "$GPGGA,230611.016,3907.3813,N,12102.4635,W,0,04,5.7,507.9,M,,,,0000*11\r\n";
+  char GPS1[] = "$GPRMC,081836,A,3751.65,S,14507.36,E,000.0,360.0,130998,011.3,E*62";
+
+  if(chksum(GPS))
+    procGPGGA(GPS);
+
+  printf("\n");
+  if(chksum(GPS1))
+    procGPRMC(GPS1);
+
+  return 0;
+}
+*/
+
+void gpsENC(char c){
+    switch (c) {
+      case '*':
+        sumStat = true;
+      case '\r':
+        break;
+      case '\n':
+        break;
+      case '$':
+      sumVal[2] = '\0';
+        buff[buffOfset] = '\0';
+        buffOfset = 0;
+        sumStat = false;
+        svIndex = 0;
+
+        if(sum == strtol(sumVal, 0, 16)){
+          gpsStat = true;
+          if(buff[3] == 'G' && buff[4] == 'A'){
+            procGPGGA(buff, &dataGPS);
+          }
+          else if(buff[3] = 'M' && buff[4] == 'C'){
+            procGPRMC(buff, &dataGPS);
+          }
+        }
+        sum = 0;
+      break;
+      default:
+        if(!sumStat){
+          buff[buffOfset++] = c;
+          sum ^= (uint8_t)c;
+        }
+        else
+          sumVal[svIndex++] = c;
+    }
+}
+
+float parseLoc(char * data, char * FLAG){
+  float result;
+
+  result = toInt(data, (FLAG - data) - 9) + atof(FLAG - 9) / 60;
+
+  if (*FLAG == 'S' || *FLAG == 'W')
+    return -result;
+
+  return result;
+}
+
+int toInt(char * data, int len){
+  int i = 0;
+  char tmp [10];
+
+  while(i < len){
+    tmp[i] = data[i];
+    i++;
+  }
+  tmp[i] = '\0';
+
+  return atoi(tmp);
+}
+
+void procGPGGA(char * data, struct dataGPS * res){
+  int index = 0;
+  float tmpF;
+  int tmpI;
+
+  char c;
+  char * ptr [20];
+
+  // Proses GPS
+  int i = 0;
+  ptr[index++] = &data[0];
+  while((c = data[i++]) != '\0'){
+    if(c == ','){
+      ptr[index++] = &data[i];
+      *(data+i-1) = '\0';
+    }
+  }
+  //TIME
+  if(ptr[1][0]){
+    res->hour = (uint8_t)toInt(ptr[1], 2);
+    res->minute = (uint8_t)toInt(ptr[1]+2, 2);
+    res->second = (uint8_t)toInt(ptr[1]+4, 2);
+  }
+
+  //Location
+  if(ptr[2][0]){
+    res->lati = parseLoc(ptr[2], ptr[3]);
+    res->longi = parseLoc(ptr[4], ptr[5]);
+  }
+
+  //FIX QUALITY
+  if(ptr[6][0])
+    res->fixQ = atoi(ptr[6]);
+
+  //SATELLITES
+  if(ptr[7][0])
+    res->sat = atoi(ptr[7]);
+
+  //ALTITUDE
+  if(ptr[9][0])
+    res->alti = atof(ptr[9]);
+}
+
+void procGPRMC(char * data, struct dataGPS * res){
+  int index = 0;
+
+  char c;
+  char * ptr [20];
+
+  // Proses GPS
+  int i = 0;
+  ptr[index++] = &data[0];
+  while((c = data[i++]) != '\0'){
+    if(c == ','){
+      ptr[index++] = &data[i];
+      *(data+i-1) = '\0';
+    }
+  }
+
+  //SPEED
+  if(ptr[7][0])
+    res->speed = atof(ptr[7]);
+
+  //HEADING
+  if(ptr[10][0])
+    res->head = atof(ptr[10]);
+}
+
+int chksum(char * data){
+  int index = 1; //skip '$'
+
+  unsigned char sum = 0;
+  char valid[3];
+  char result[3];
+
+  unsigned char c;
+  while((c = data[index++]) != '*')
+    sum ^= c;
+
+  valid[0] = data[index++];
+  valid[1] = data[index++];
+  valid[2] = '\0';
+
+  sprintf(result, "%x", sum);
+
+  if(!strcmp(valid, result))
+    return 1;
+
+  return 0;
+}
